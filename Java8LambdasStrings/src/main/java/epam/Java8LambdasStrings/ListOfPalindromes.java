8eb283184204eb1790221af18f180154796e8161
ArepalliSindhura <17wh1a05a6@bvrithyderabad.edu.in>
2020-07-24 18:36:32 +0530

    Committing changes

--- Java8LambdasStrings/src/main/java/epam/Java8LambdasStrings/ListOfPalindromes.java
+++ Java8LambdasStrings/src/main/java/epam/Java8LambdasStrings/ListOfPalindromes.java
@@ -1,10 +1,35 @@
 package epam.Java8LambdasStrings;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class ListOfPalindromes {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		ArrayList<String> al = new ArrayList<String>();
+		ArrayList<String> ans = new ArrayList<String>();
+		System.out.println("enter the number of strings you wish to store..");
+		int len = sc.nextInt();
+		System.out.println("enter the strings");
+		for(int i = 0; i < len; i++) {
+			al.add(sc.next());
+		}
+		ans = palindromeList(al);
+		System.out.println("The palindrome strings are ");
+		System.out.println(ans);
+	}
+ 
+	private static ArrayList<String> palindromeList(ArrayList<String> al) {
+		return (ArrayList<String>) al.stream().filter(str -> str.equals(ReverseString(str))).collect(Collectors.toList());
 	}
 
+	public static String ReverseString(String str) {
+		String s = "";
+		for(int i = str.length()-1; i >= 0; i--) {
+			s += str.charAt(i);
+		}
+		return s;
+	}
 }
--- Java8LambdasStrings/src/main/java/epam/Java8LambdasStrings/StringsWithLetterA.java
+++ Java8LambdasStrings/src/main/java/epam/Java8LambdasStrings/StringsWithLetterA.java
@@ -1,10 +1,28 @@
 package epam.Java8LambdasStrings;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class StringsWithLetterA {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		ArrayList<String> al = new ArrayList<String>();
+		ArrayList<String> ans = new ArrayList<String>();
+		System.out.println("enter the number of strings you wish to store..");
+		int len = sc.nextInt();
+		System.out.println("enter the strings");
+		for(int i = 0; i < len; i++) {
+			al.add(sc.next());
+		}
+		ans = StringList(al);
+		System.out.println("The strings are ");
+		ans.forEach(x -> System.out.println(x));
+	}
 
+	private static ArrayList<String> StringList(ArrayList<String> al) {
+		return (ArrayList<String>) al.stream().filter(str -> str.length() == 3).filter(str -> str.startsWith("a")).collect(Collectors.toList());
 	}
 
 }
